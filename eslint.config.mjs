818ad85d5d78fbca2118b0c38b839ddd8df7411e
brutal-45 <creatorsports81@gmail.com>
2026-05-20 16:32:29 +0530

    Fix import statement for @eslint/js module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { FlatCompat } from '@eslint/eslintrc'
 import { fileURLToPath } from 'node:url'
 import globals from 'globals'
 import path from 'node:path'
-import js from '@eslint/js'
+import js from '@eslint/js' 
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
